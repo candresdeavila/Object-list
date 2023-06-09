5078550ef4f5567c9e758db73969c73e59e9f747
Cristian De Avila <115848481+candresdeavila@users.noreply.github.com>
2023-06-09 08:07:20 -0500

    Add files via upload

---
+++ guitars.js
@@ -0,0 +1,28 @@
+let guitarras = [
+    {
+    nombre: "Stratocaster",
+    marca: "Fender",
+    color: "Blanco",
+    artistas: ["Eric Clapton", "Jimi Hendrix", "David Gilmour"]
+    },
+    {
+    nombre: "SG",
+    marca: "Gibson",
+    color: "Negro",
+    artistas: ["Angus Young", "Tony Iommi", "Derek Trucks"]
+    },
+    {
+    nombre: "RG",
+    marca: "Ibanez",
+    color: "Azul",
+    artistas: ["Steve Vai", "Joe Satriani", "Paul Gilbert"]
+    },
+    {
+    nombre: "Pacific",
+    marca: "Yamaha",
+    color: "Rojo",
+    artistas: ["Dave Mustaine", "Marty Friedman", "John Petrucci"]
+    }
+    ];
+    
+    console.log(guitarras);
